478c1a3bea9a1fce8a34bfa7143be9f2543d461c
rheech <lee@cheonghyun.com>
2016-11-04 09:09:59 +0900

    Remove unnecessary tool

--- Tools/python_mklink_debug.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0"
-mklink /d "..\IpcPythonCS.UI\bin\Debug\Python\" "..\..\..\IpcPythonCS.Engine.Python\"
-mklink /d "..\IPCPythonCS.Test.Engine.CSharp\bin\Debug\Python\" "..\..\..\IpcPythonCS.Engine.Python\"
-pause
